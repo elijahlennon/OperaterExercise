907698c14c77317c93c58ccfc3097b7b2058ae45
Elijah Lennon <elijahlennon1@gmail.com>
2022-06-01 10:57:10 -0500

    created simple program

---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace OperatorExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //for division, create two interger variables called a and b
+
+            int a = 17; //decleration  & initialization
+            int b = 4;
+            var qoutient = a / b;
+            var remainder = a % b;
+
+            Console.WriteLine($"{a} / {b} = {qoutient} remainder {remainder}");
+
+            Console.WriteLine("Please enter the radius of your circle:");
+            var userInput = Console.ReadLine();
+
+            var radius = double.Parse(Console.ReadLine()); 
+
+            var area = Math.PI * Math.Pow(radius, 2);
+
+            Console.WriteLine($"The area of a circle with a radius of {radius} is {area}.");
+
+        }
+
+        
+
+
+          
+
+        
+    }
+}
